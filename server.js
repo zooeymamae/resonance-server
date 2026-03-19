2e55faa448a2e81a4a46927352c8cc36daaf5cee
zooeymamae <147275638+zooeymamae@users.noreply.github.com>
2026-03-19 00:57:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -89,4 +89,4 @@ app.get('/search/movies', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✓ Resonance server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`✓ Resonance server running on port ${PORT}`));
